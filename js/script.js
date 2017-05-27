e08f162c52d98accfeae26b4e4d629ced68ff161
Durin Vladislav <rock_n_vladok@mail.ru>
2017-05-27 15:14:37 +0400

    Added a new function reblur and added events mouseover, mouseout

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var init = function () {
     var image = document.getElementsByTagName('img');
     for (var i = 0; i < image.length; i++) {
-        image[i].addEventListener('click', showAnswer);
+        image[i].addEventListener('mouseover', showAnswer);
+        image[i].addEventListener('mouseout', reblur);        
     }
 };
 var showAnswer = function (evt) {
@@ -10,5 +11,10 @@ var showAnswer = function (evt) {
     name = name + '.jpg';
     image.src = 'img/' + name;
 };
-
+var reblur = function (evt) {
+    var image = evt.target;
+    var name = image.id;
+    name = name + 'blur.jpg';
+    image.src = 'img/' + name; 
+};
 window.onload = init;
